34412bf4853c0224e300b95b9619b6b10ad6902a
Oleh Paduchak <opaduchak@exoft.net>
2025-02-27 13:49:14 +0200

    implemented VOL fro GV
    Co-authored-by: NAME <sheredko.andriy@gmail.com>

--- addon_service/resource_reference/views.py
+++ addon_service/resource_reference/views.py
@@ -2,6 +2,7 @@ from addon_service.common.permissions import SessionUserCanViewReferencedResourc
 from addon_service.common.viewsets import RestrictedReadOnlyViewSet
 from addon_service.serializers import ResourceReferenceSerializer
 
+from ..common.view_only_filter import ViewOnlyFilter
 from .models import ResourceReference
 
 
@@ -9,5 +10,18 @@ class ResourceReferenceViewSet(RestrictedReadOnlyViewSet):
     queryset = ResourceReference.objects.all()
     serializer_class = ResourceReferenceSerializer
     permission_classes = [SessionUserCanViewReferencedResource]
+    filter_backends = [ViewOnlyFilter]  # Ensures `view_only` is always allowed
     # Satisfies requirements of `RestrictedReadOnlyViewSet.list`
     required_list_filter_fields = ("resource_uri",)
+
+    def get_query_parameters(self):
+        params = super().get_query_parameters()
+        params.add("view_only")  # Allow this custom parameter
+        return params
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if "view_only" in self.request.query_params:
+            # Apply readonly logic if needed
+            pass
+        return queryset
