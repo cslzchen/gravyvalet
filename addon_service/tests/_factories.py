8f7c9a84572cc993a2f87c112e2200d3daa9eadc
Vlad0n20 <vlad.onischuk1234@gmail.com>
2025-04-16 13:27:29 +0300

    Add new factories for link addons

--- addon_service/tests/_factories.py
+++ addon_service/tests/_factories.py
@@ -187,3 +187,148 @@ class ConfiguredStorageAddonFactory(DjangoModelFactory):
             *args,
             **kwargs,
         )
+
+
+###
+# "Link" models
+
+
+class ExternalLinkServiceFactory(DjangoModelFactory):
+    class Meta:
+        model = db.ExternalLinkService
+
+    display_name = factory.Faker("word")
+    int_addon_imp = known_imps.get_imp_number(
+        known_imps.get_imp_by_name("LINK_DATAVERSE")
+    )
+    supported_scopes = ["service.url/grant_all"]
+
+    @classmethod
+    def _create(
+        cls,
+        model_class,
+        credentials_format=CredentialsFormats.PERSONAL_ACCESS_TOKEN,
+        service_type=ServiceTypes.PUBLIC,
+        supported_resource_types=None,
+        *args,
+        **kwargs,
+    ):
+        from addon_toolkit.interfaces.link import SupportedResourceTypes
+
+        api_base_url = ""
+        if ServiceTypes.PUBLIC in service_type:
+            api_base_url = "https://api.example.url/v1"
+
+        supported_resource_types = (
+            supported_resource_types or SupportedResourceTypes.DATASET
+        )
+
+        return super()._create(
+            model_class=model_class,
+            int_credentials_format=credentials_format.value,
+            int_service_type=service_type.value,
+            int_supported_resource_types=supported_resource_types.value,
+            api_base_url=api_base_url,
+            *args,
+            **kwargs,
+        )
+
+
+class ExternalLinkOAuth2ServiceFactory(ExternalLinkServiceFactory):
+    credentials_format = CredentialsFormats.OAUTH2
+    oauth2_client_config = factory.SubFactory(OAuth2ClientConfigFactory)
+
+
+class ExternalLinkOAuth1ServiceFactory(ExternalLinkServiceFactory):
+    credentials_format = CredentialsFormats.OAUTH1A
+    oauth1_client_config = factory.SubFactory(OAuth1ClientConfigFactory)
+
+
+class AuthorizedLinkAccountFactory(DjangoModelFactory):
+    class Meta:
+        model = db.AuthorizedLinkAccount
+
+    display_name = factory.Faker("word")
+    authorized_capabilities = AddonCapabilities.ACCESS | AddonCapabilities.UPDATE
+
+    @classmethod
+    def _create(
+        cls,
+        model_class,
+        account_owner=None,
+        external_service=None,
+        credentials=None,
+        credentials_format=CredentialsFormats.OAUTH2,
+        authorized_scopes=None,
+        *args,
+        **kwargs,
+    ):
+        from addon_service.tests._helpers import patch_encryption_key_derivation
+
+        account = super()._create(
+            model_class=model_class,
+            external_service=external_service
+            or ExternalLinkOAuth2ServiceFactory(credentials_format=credentials_format),
+            account_owner=account_owner or UserReferenceFactory(),
+            *args,
+            **kwargs,
+        )
+        if credentials_format is CredentialsFormats.OAUTH2:
+            account.initiate_oauth2_flow(authorized_scopes)
+        else:
+            with patch_encryption_key_derivation():
+                from addon_service.tests.test_by_type.test_authorized_link_account import (
+                    MOCK_CREDENTIALS,
+                )
+
+                account.credentials = credentials or MOCK_CREDENTIALS.get(
+                    credentials_format
+                )
+            account.save()
+        return account
+
+
+class ConfiguredLinkAddonFactory(DjangoModelFactory):
+    class Meta:
+        model = db.ConfiguredLinkAddon
+
+    target_id = factory.Faker("uuid4")
+    connected_capabilities = AddonCapabilities.ACCESS
+
+    @classmethod
+    def _create(
+        cls,
+        model_class,
+        authorized_resource=None,
+        external_link_service=None,
+        credentials_format=CredentialsFormats.OAUTH2,
+        base_account=None,
+        account_owner=None,
+        credentials=None,
+        resource_type=None,
+        *args,
+        **kwargs,
+    ):
+        from addon_toolkit.interfaces.link import SupportedResourceTypes
+
+        authorized_resource = authorized_resource or ResourceReferenceFactory()
+        base_account = base_account or AuthorizedLinkAccountFactory(
+            external_service=external_link_service,
+            credentials_format=credentials_format,
+            account_owner=account_owner,
+            credentials=credentials,
+        )
+
+        resource_type = resource_type or SupportedResourceTypes.DATASET
+
+        kwargs["int_resource_type"] = resource_type.value
+
+        addon = super()._create(
+            model_class=model_class,
+            authorized_resource=authorized_resource,
+            base_account=base_account,
+            *args,
+            **kwargs,
+        )
+
+        return addon
