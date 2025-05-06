16521c47af98ba05ac6c5679fe82eff1094e3dd1
Oleh Paduchak <opaduchak@exoft.net>
2025-05-06 13:18:20 +0300

    fixed Hybrid  service type not being displayed in admin

--- addon_service/admin/_base.py
+++ addon_service/admin/_base.py
@@ -56,7 +56,7 @@ class GravyvaletModelAdmin(admin.ModelAdmin):
                 label=db_field.verbose_name,
                 choices=[
                     (None, ""),
-                    *((_member.value, _member.name) for _member in _enum),
+                    *self.iter_enum_members(_enum),
                 ],
             )
         if (
@@ -65,11 +65,17 @@ class GravyvaletModelAdmin(admin.ModelAdmin):
         ):
             _enum = self.enum_multiple_choice_fields[db_field.name]
             return EnumNameMultipleChoiceField(
-                choices=[
-                    *((_member.value, _member.name) for _member in _enum),
-                ],
+                choices=self.iter_enum_members(_enum),
                 widget=forms.CheckboxSelectMultiple,
                 enum_cls=_enum,
             )
 
         return super().formfield_for_dbfield(db_field, request, **kwargs)
+
+    def iter_enum_members(self, _enum):
+        if hasattr(_enum, "__members__"):
+            iterator = _enum.__members__.values()
+        else:
+            iterator = _enum
+
+        return [(_member.value, _member.name) for _member in iterator]
