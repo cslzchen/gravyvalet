1b3516f2699e21852c1b6dd0b9532f1d9fc78327
Longze Chen <cslzchen@gmail.com>
2025-05-27 10:06:40 -0400

    Add int_supported_features to link type external service

--- addon_service/external_service/link/models.py
+++ addon_service/external_service/link/models.py
@@ -1,3 +1,8 @@
+from enum import (
+    Flag,
+    auto,
+)
+
 from django.core.exceptions import ValidationError
 from django.db import models
 
@@ -10,19 +15,51 @@ from addon_service.external_service.models import ExternalService
 from addon_toolkit.interfaces.link import SupportedResourceTypes
 
 
+# TODO: need to trim down the features
+class LinkSupportedFeatures(Flag):
+    ADD_UPDATE_FILES = auto()
+    ADD_UPDATE_FILES_PARTIAL = auto()
+    DELETE_FILES = auto()
+    DELETE_FILES_PARTIAL = auto()
+    FORKING = auto()
+    LOGS = auto()
+    PERMISSIONS = auto()
+    REGISTERING = auto()
+    FILE_VERSIONS = auto()
+    COPY_INTO = auto()
+    DOWNLOAD_AS_ZIP = auto()
+
+
 def validate_supported_features(value):
+    _validate_enum_value(LinkSupportedFeatures, value)
+
+
+def validate_supported_resource_types(value):
     _validate_enum_value(SupportedResourceTypes, value)
 
 
 class ExternalLinkService(ExternalService):
     browser_base_url = models.URLField(blank=True, default="")
-    int_supported_resource_types = models.BigIntegerField(
+    int_supported_features = models.IntegerField(
         validators=[validate_supported_features], null=True
+    )
+    int_supported_resource_types = models.BigIntegerField(
+        validators=[validate_supported_resource_types], null=True
     )
 
     @property
-    def supported_resource_types(self) -> list[SupportedResourceTypes]:
+    def supported_features(self) -> list[LinkSupportedFeatures]:
         """get the enum representation of int_supported_features"""
+        return LinkSupportedFeatures(self.int_supported_features)
+
+    @supported_features.setter
+    def supported_features(self, new_supported_features: LinkSupportedFeatures):
+        """set int_authorized_capabilities without caring its int"""
+        self.int_supported_features = new_supported_features.value
+
+    @property
+    def supported_resource_types(self) -> list[SupportedResourceTypes]:
+        """get the enum representation of int_supported_resource_types"""
         return SupportedResourceTypes(self.int_supported_resource_types)
 
     @supported_resource_types.setter
